7f2536cf966c83a94fcf0557dc1c66a9f50ababd
E-Cast <emilcastonguay@gmail.com>
2023-03-03 23:47:20 -0500

    bzero memset cleanup

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,14 +1,14 @@
 #include <stddef.h>
 void	ft_bzero(void* s, size_t n)
 {
-	unsigned	char *p;
+	unsigned	char *str;
 	size_t	i;
 
-	p = s;
+	str = s;
 	i = 0;
 	while (i < n)
 	{
-		p[i] = 0;
+		str[i] = 0;
 		i++;
 	}
 }
--- ft_memset.c
+++ ft_memset.c
@@ -1,14 +1,14 @@
 #include <stddef.h>
 void	ft_memset(void* s, int ch , size_t n)
 {
-	unsigned	char *p;
+	unsigned	char *str;
 	size_t	i;
 
-	p = s;
+	str = s;
 	i = 0;
 	while (i < n)
 	{
-		p[i] = ch;
+		str[i] = ch;
 		i++;
 	}
 }
