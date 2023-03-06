44362df908e3e3d021ac6a6c41d834db04db4fbf
E-Cast <emilcastonguay@gmail.com>
2023-03-06 16:31:14 -0500

    .\ft_isprint.c norminette

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,4 +1,4 @@
-int	ft_isprint(int	c)
+int	ft_isprint(int c)
 {
 	if (c >= 32 && c <= 126)
 		return (1);
@@ -11,7 +11,8 @@ int	ft_isprint(int	c)
 // int	main(void)
 // {
 // 	for (int i = 0; i <= 127; i++)
-// 		printf("%c, %d, isprint: %d, ft_isprint: %d\n", i, i, isprint(i), ft_isprint(i));
+// 		printf("%c, %d, isprint: %d, ft_isprint: %d\n", 
+// 				i, i, isprint(i), ft_isprint(i));
 
 // 	return (0);
 // }
