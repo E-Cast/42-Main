5cfde823c86bfd0364e0e5b85f48a297b45a9645
E-Cast <emilcastonguay@gmail.com>
2023-03-06 16:26:20 -0500

    .\ft_isalnum.c norminette

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,6 +1,8 @@
-int	ft_isalnum(int	c)
+int	ft_isalnum(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
@@ -12,7 +14,8 @@ int	ft_isalnum(int	c)
 // {
 
 // 	for (int i = 0; i <= 127; i++)
-// 		printf("%c, %d, isalnum: %d, ft_isalnum: %d\n", i, i, ft_isalnum(i), ft_isalnum(i));
+// 		printf("%c, %d, isalnum: %d, ft_isalnum: %d\n",
+// 				i, i, ft_isalnum(i), ft_isalnum(i));
 
 // 	return (0);
 // }
