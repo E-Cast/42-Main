5c159fd8b773ee92716fdfc51da6231d1dfce0b3
E-Cast <emilcastonguay@gmail.com>
2023-03-06 16:21:37 -0500

    .\ft_isdigit.c norminette

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,6 +1,6 @@
-int	ft_isdigit(int	c)
+int	ft_isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
@@ -11,7 +11,8 @@ int	ft_isdigit(int	c)
 // int	main(void)
 // {
 // 	for (int i = 0; i <= 127; i++)
-// 		printf("%c, %d, isdigit: %d, ft_isdigit: %d\n", i, i, isdigit(i), ft_isdigit(i));
+// 		printf("%c, %d, isdigit: %d, ft_isdigit: %d\n", 
+// 				i, i, isdigit(i), ft_isdigit(i));
 
 // 	return (0);
 // }
