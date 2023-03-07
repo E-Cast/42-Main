7a7efe23d2d1553aac8aaf06200c08de1119b275
E-Cast <emilcastonguay@gmail.com>
2023-03-06 23:57:43 -0500

    ft_memmove working with ints

--- ft_memmove.c
+++ ft_memmove.c
@@ -20,13 +20,25 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 // 	char s1[] = "Hello World";
 // 	char s2[] = "Hello World";
 
-// 	// use standard memmove
 // 	memmove(&s1[3], &s1[0], 5);
-// 	printf("memmove: %s\n", s1);
-
-// 	// use your implementation
+// 	printf("memmove:%s\n", s1);
 // 	ft_memmove(&s2[3], &s2[0], 5);
-// 	printf("ft_memmove: %s\n", s2);
+// 	printf("ft_memmove:%s\n", s2);
 
+// 	int arr1[] = {1, 2, 3, 4, 5};
+// 	int arr2[] = {1, 2, 3, 4, 5};
+
+// 	memmove(&arr1[2], &arr1[0], 3 * sizeof(int));
+// 	printf("memmove:");
+// 	for (size_t i = 0; i < 5; i++)
+// 	{
+// 		printf("%d ", arr1[i]);
+// 	}
+// 	ft_memmove(&arr2[2], &arr2[0], 3 * sizeof(int));
+// 	printf("\nft_memmove:");
+// 	for (size_t i = 0; i < 5; i++)
+// 	{
+// 		printf("%d ", arr2[i]);
+// 	}
 // 	return (0);
 // }
