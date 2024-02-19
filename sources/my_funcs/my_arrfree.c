5d25884c85efbba503993c2b8ebae97be071389e
E-Cast <emilcastonguay@gmail.com>
2024-02-19 03:04:53 -0500

    modified my_arrfree

--- sources/my_funcs/my_arrfree.c
+++ sources/my_funcs/my_arrfree.c
@@ -6,19 +6,19 @@
 /*   By: ecast <ecast@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 02:48:51 by ecast             #+#    #+#             */
-/*   Updated: 2024/02/19 02:50:10 by ecast            ###   ########.fr       */
+/*   Updated: 2024/02/19 03:04:33 by ecast            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*Safely frees the array and it's content, returns NULL*/
-void	*my_freearr(void **array)
+void	*my_freearr(void **array, size_t size)
 {
-	int	index;
+	size_t	index;
 
 	index = 0;
-	while (array && array[index])
+	while (index < size)
 		my_safefree(array[index++]);
 	my_safefree(array);
 	array = NULL;
