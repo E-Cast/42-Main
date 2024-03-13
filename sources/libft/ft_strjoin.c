188e2af5b4fbb037361a24576c058241b9e7bd5d
E-Cast <emilcastonguay@gmail.com>
2024-03-13 14:17:56 -0400

    named dtrjoin correclty

--- sources/libft/ft_strjoin.c
+++ sources/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:12:47 by ecastong          #+#    #+#             */
-/*   Updated: 2024/03/13 14:16:57 by ecastong         ###   ########.fr       */
+/*   Updated: 2024/03/13 14:17:45 by ecastong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*Allocates and returns a new string, which is the 
 	result of the concatenation of str1 and str2.*/
-char	*my_strjoin(const char *str1, const char *str2)
+char	*ft_strjoin(const char *str1, const char *str2)
 {
 	char	*dest;
 	size_t	dest_len;
