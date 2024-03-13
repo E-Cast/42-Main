b6ad673e43061b721a2aa73c314136d8484abea1
E-Cast <emilcastonguay@gmail.com>
2024-03-13 14:17:31 -0400

    upgraded strjoin to use memcpy

--- sources/libft/ft_strjoin.c
+++ sources/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 19:12:47 by ecastong          #+#    #+#             */
-/*   Updated: 2024/02/06 15:23:53 by ecastong         ###   ########.fr       */
+/*   Updated: 2024/03/13 14:16:57 by ecastong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,19 @@
 
 /*Allocates and returns a new string, which is the 
 	result of the concatenation of str1 and str2.*/
-char	*ft_strjoin(const char *str1, const char *str2)
+char	*my_strjoin(const char *str1, const char *str2)
 {
 	char	*dest;
-	size_t	i;
-	size_t	j;
 	size_t	dest_len;
 
 	if (!str1 || !str2)
 		return (NULL);
-	dest_len = ft_strlen(str1) + ft_strlen(str2) + 1;
-	dest = (char *)ft_calloc(dest_len, sizeof(char));
+	dest_len = ft_strlen(str1) + ft_strlen(str2);
+	dest = (char *)ft_calloc(dest_len + 1, sizeof(char));
 	if (!dest)
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (str1[i])
-	{
-		dest[i] = str1[i];
-		i++;
-	}
-	while (str2[j])
-		dest[i++] = str2[j++];
-	dest[i] = '\0';
+	ft_memcpy(dest, str1, ft_strlen(str1));
+	ft_memcpy(dest + ft_strlen(dest), str2, ft_strlen(str2));
+	dest[dest_len] = '\0';
 	return (dest);
 }
