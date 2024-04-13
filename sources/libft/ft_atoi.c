535524eb65c4cf5c09ee9114b2a835bf7ee19588
E-Cast <emilcastonguay@gmail.com>
2024-04-13 01:43:31 -0400

    fixed small bug in atoi

--- sources/libft/ft_atoi.c
+++ sources/libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 23:00:42 by ecastong          #+#    #+#             */
-/*   Updated: 2024/04/13 01:41:40 by ecastong         ###   ########.fr       */
+/*   Updated: 2024/04/13 01:43:15 by ecastong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	ft_atoi(const char *str)
 		i++;
 	if (str[i] && str[i] == '-')
 		sign = -1;
-	if (str[i] && str[i] == '+' || str[i] == '-')
+	if (str[i] && (str[i] == '+' || str[i] == '-'))
 		i++;
 	while (str[i] && (ft_isdigit(str[i])))
 	{
